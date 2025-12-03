1f6ffba573c843190e4c165c42df0416401c184b
GioMjds <giomjds@gmail.com>
2025-12-03 13:24:57 +0800

    monorepo init

--- shared/gateways/Message.ts
+++ shared/gateways/Message.ts
@@ -1,7 +1,8 @@
 import { httpClient } from "utilities/axios";
+import { Response } from "../types/Message.types.js";
 
 export const message = {
-    async msg() {
+    async msg(): Promise<Response> {
         return await httpClient.get('/');
     }
 }
